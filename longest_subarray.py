19f3896d8fb7fce5fd949f2819e271526a5aec62
diink-iis <dii.grase@yandex.ru>
2025-03-02 21:54:21 +0300

    leetcode: python

---
+++ longest_subarray.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        ans = zeros = left = 0
+        for i in range(len(nums)):
+            zeros += (nums[i] == 0)
+            while zeros > 1:
+                zeros -= (nums[left] == 0)
+                left += 1
+            ans = max(ans, i-left)
+        return ans
--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from longest_substring import Solution
 from find_median import Solution
 from palindrome_substring import Solution
 from summary_ranges import Solution
+from longest_subarray import Solution
 
 def main():
-    print(Solution.summaryRanges([]))
+    print(Solution.longestSubarray([1,1,0,1]))
 
 if __name__ == '__main__':
     main()
