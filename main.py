21272f4c18fdf45d187d04787db2f31ca10b8d81
diink-iis <dii.grase@yandex.ru>
2025-03-04 22:37:57 +0300

    leetcode: python

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from easy.palindrome_substring import Solution
 from medium.summary_ranges import Solution
 from medium.longest_subarray import Solution
 from easy.first_unique_char import Solution
+from medium.reverce_int import Solution
 
 def main():
-    print(Solution.firstUniqChar("aabb"))
+    print(Solution.reverse(321))
 
 if __name__ == '__main__':
     main()
